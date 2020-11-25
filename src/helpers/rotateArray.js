95817bcdb33cdfd552646e2c24aa554f2d5b9f66
Kirill Verenich <kirillverenih@ya.ru>
2020-11-25 21:19:29 +0300

    add test for rotateArray

--- src/helpers/rotateArray.js
+++ src/helpers/rotateArray.js
@@ -1,5 +1,8 @@
+
+// shifts array to the left n times
 export const rotateArray = (array, amount) => {
   for (let i = 0; i < amount; i++) {
+    console.log(array)
     array.push(array.shift());
   }
 
---
+++ src/helpers/rotateArray.test.js
@@ -0,0 +1,7 @@
+import { rotateArray } from './rotateArray';
+
+it('rotates array', () => {
+  const initialArray = ['1', '2', '3', '4', '5', ];
+  let rotatedArray = rotateArray(initialArray, 2);
+  expect(rotatedArray).toEqual(['3', '4', '5', '1', '2', ])
+});
