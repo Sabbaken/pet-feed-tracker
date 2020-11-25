3f37326c5276810ebfde32c8b5ec76fb0fa1b1fa
Kirill Verenich <kirillverenih@ya.ru>
2020-11-25 21:19:20 +0300

    add test for sortFeedsByDaysOfWeek

---
+++ src/helpers/sortFeedsByDaysOfWeek.test.js
@@ -0,0 +1,43 @@
+import { sortFeedsByDaysOfWeek } from './sortFeedsByDaysOfWeek';
+import { rotateArray } from './rotateArray';
+
+it('sortFeedsByDaysOfWeek sorts in correct way', () => {
+  let today = new Date();
+  const feeds = [{ timestamp: today.valueOf(), amount: 1 }];
+  let feedsByDaysCorrect = [
+    { name: 'Вс', feeds: [] },
+    { name: 'Пн', feeds: [] },
+    { name: 'Вт', feeds: [] },
+    { name: 'Ср', feeds: [] },
+    { name: 'Чт', feeds: [] },
+    { name: 'Пт', feeds: [] },
+    { name: 'Сб', feeds: [] },
+  ];
+  feedsByDaysCorrect[today.getDay()].feeds = feeds;
+  feedsByDaysCorrect = rotateArray(feedsByDaysCorrect, today.getDay() + 1);
+
+  let feedsByDays = sortFeedsByDaysOfWeek(feeds);
+
+  expect(feedsByDays).toEqual(feedsByDaysCorrect)
+});
+
+it('sortFeedsByDaysOfWeek excludes timestamps older than week', () => {
+  let today = new Date();
+  let moreThanWeekFromNow = new Date(today.getTime() - (8 * 24 * 60 * 60 * 1000));
+  const feeds = [{ timestamp: moreThanWeekFromNow.valueOf(), amount: 1 }];
+
+  let feedsByDaysCorrect = [
+    { name: 'Вс', feeds: [] },
+    { name: 'Пн', feeds: [] },
+    { name: 'Вт', feeds: [] },
+    { name: 'Ср', feeds: [] },
+    { name: 'Чт', feeds: [] },
+    { name: 'Пт', feeds: [] },
+    { name: 'Сб', feeds: [] },
+  ];
+  feedsByDaysCorrect = rotateArray(feedsByDaysCorrect, today.getDay() + 1);
+
+  let feedsByDays = sortFeedsByDaysOfWeek(feeds);
+
+  expect(feedsByDays).toEqual(feedsByDaysCorrect)
+});
